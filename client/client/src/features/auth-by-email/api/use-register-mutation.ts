3d6af5e483cb65042157bb49708ee21b962f69be
Platon <kplatonglfc@gmail.com>
2026-08-15 13:10:17 +0600

    feat(client): save auth state on successful register mutation

--- client/client/src/features/auth-by-email/api/use-register-mutation.ts
+++ client/client/src/features/auth-by-email/api/use-register-mutation.ts
@@ -2,14 +2,19 @@ import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { registerRequest } from "./auth-api";
 import { PayloadRegister, AuthResponse } from "../model/types";
+import { useAuthStore } from "@/shared/store/use-auth-store";
 
 export const useRegisterMutation = () => {
   const router = useRouter();
+  const setAuth = useAuthStore((state)=> state.setAuth)
 
   return useMutation({
     mutationFn: (payload: PayloadRegister) => registerRequest(payload),
     onSuccess: (data: AuthResponse) => {
-      // TODO: Сохранить токен в zustand
+      setAuth({
+        user: data.user,
+        accessToken:data.accessToken
+      })
       router.replace("/dashboard");
     },
   });
