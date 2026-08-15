09928b4a12fc91875ce788eef1c739fc6fa9b8f1
Platon <kplatonglfc@gmail.com>
2026-08-15 13:10:14 +0600

    feat(client): save auth state on successful login mutation

--- client/client/src/features/auth-by-email/api/use-login-mutation.ts
+++ client/client/src/features/auth-by-email/api/use-login-mutation.ts
@@ -1,15 +1,20 @@
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { loginRequest } from "./auth-api";
-import { PayloadLogin,AuthResponse } from "../model/types";
+import { PayloadLogin, AuthResponse } from "../model/types";
+import { useAuthStore } from "@/shared/store/use-auth-store";
 
 export const useLoginMutation = () => {
   const router = useRouter();
+  const setAuth = useAuthStore((state) => state.setAuth);
 
   return useMutation({
     mutationFn: (payload: PayloadLogin) => loginRequest(payload),
-    onSuccess: (data:AuthResponse) => {
-      // TODO: Сохранить data.accessToken в zustand стор
+    onSuccess: (data: AuthResponse) => {
+      setAuth({
+        user: data.user,
+        accessToken: data.accessToken,
+      })
       router.replace("/dashboard");
     },
   });
